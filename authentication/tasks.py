4e647aacc080399dd775afc7f0e6266202bf96a4
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-27 01:19:46 +0530

    added some time interval to mimic real life systems

--- authentication/tasks.py
+++ authentication/tasks.py
@@ -3,10 +3,12 @@ from django.core.cache import cache
 import random
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+import time
 
 @shared_task
 def OtpCreateTask(username):
     otp=random.randint(100000, 999999)
+    time.sleep(10)
     user_data=get_object_or_404(User, username=username)
     cache.set(f"otp_for_user:{user_data.id}", str(otp), timeout=500)
     return f"otp created for username: {username}, otp: {otp}"
