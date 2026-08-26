2181b8f5339ae08bc4bab67ce06a537b38c3b025
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-26 20:44:02 +0530

    seperated the workload

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    name=models.CharField()
+    profile_picture=models.ImageField(upload_to='pfps/', null=True)
+    bio=models.TextField()
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.name=f"{self.user.first_name} {self.user.last_name}"
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
--- core/models.py
+++ core/models.py
@@ -1,19 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from authentication.models import Profile
 
-class Profile(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
-    name=models.CharField()
-    profile_picture=models.ImageField(upload_to='pfps/', null=True)
-    bio=models.TextField()
-    created_on=models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        self.name=f"{self.user.first_name} {self.user.last_name}"
-        return super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
 
 class Blog(models.Model):
     user=models.ForeignKey(Profile, on_delete=models.CASCADE)
