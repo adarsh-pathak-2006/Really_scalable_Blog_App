83805506c69dbf329a807d042861f6dfff946a07
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-30 15:02:37 +0530

    fixed minor import issue in the url in the authentication app

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import CustomTokenObtainView, CustomTokenRefreshView, RegisterAPI, OtpVerificationAPI, MyProfileAPI
+from .views import CustomTokenObtainView, CustomTokenRefreshView, RegisterAPI, MyProfileAPI
 
 urlpatterns = [
     path('token/', CustomTokenObtainView.as_view(), name='token_obtain_view'),
     path('register-user/', RegisterAPI.as_view(), name='register'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh_view'),
-    path('otp-verify/<int:username>/', OtpVerificationAPI.as_view(), name='otp_verification'),
     path('my-profile/', MyProfileAPI.as_view(), name='my_profile'),
 ]
